e501cf73ed78d925dc8b2b7f29e9358bdb4d54f6
Yang Fei <ccc420513@gmail.com>
2024-01-19 17:01:35 +0800

    条件语句

---
+++ 01-python-fundamentals/basicLearn/ifLearn.py
@@ -0,0 +1,8 @@
+print("********欢迎进入登录界面******")
+username=input("Enter your username: ")
+passward=input("Enter your password: ")
+if username=="root" and passward=="12345":
+    print("登录成功!")
+else:
+    print("用户名或密码错误!")
+print("********程序结束******")
