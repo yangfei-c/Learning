69df5149cb25618f49b9a8f9410c1855e125301b
YangFei <3260693417@qq.com>
2024-11-08 15:50:37 +0800

    开始学习python

--- 01-python-fundamentals/debugg/py/debugg.py
+++ 01-python-fundamentals/debugg/py/debugg.py
@@ -5,8 +5,16 @@
 # @Email   : ccc420513@gmail.com
 # @File    : debugg.py
 # @Software: PyCharm
-def spam():
-    bacon()
-def bacon():
-    raise Exception('The is  the error message')
-spam()
+import logging
+logging.basicConfig(filename='mylog',level=logging.DEBUG,format='%(asctime)s -%(levelname)s - %(message)s')
+logging.debug('Start of program')
+def factorial(n):
+    logging.debug(f'Start of factorial({n})' )
+    total=1
+    for i in range(1,n+1):
+        total *=i
+        logging.debug('i is '+str(i)+',total is '+str(total))
+    logging.debug(f'End of factorial({n})')
+    return total
+print(factorial(5))
+logging.debug('End of program')
